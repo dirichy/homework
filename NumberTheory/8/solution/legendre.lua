ccff02e20eebc114fff9a13698dcbe5475eeb67b
dirichy <144102919+dirichy@users.noreply.github.com>
2024-07-04 22:15:45 +0800

    update

--- NumberTheory/8/solution/legendre.lua
+++ NumberTheory/8/solution/legendre.lua
@@ -1,3 +1,11 @@
+local function isprime(n)
+	for i = 2, n - 1 do
+		if n % i == 0 then
+			return false
+		end
+	end
+	return true
+end
 local function legendre(p, q)
 	if p % q == 0 then
 		return 0
@@ -16,9 +24,12 @@ local function legendre(p, q)
 	return -1
 end
 local function defactor(n)
-	local result = {}
+	local result = { isprime = true }
 	for i = 2, n, 1 do
 		if n % i == 0 then
+			if i < n then
+				result.isprime = false
+			end
 			result[i] = 0
 			while n % i == 0 do
 				result[i] = result[i] + 1
@@ -42,7 +53,7 @@ local function termnext(term)
 			return { { type = "number", value = (term.lower * term.lower - 1) % 16 == 0 and 1 or -1 } }
 		end
 		local factors = defactor(term.upper)
-		if #factors == 1 then
+		if factors.isprime then
 			state = {
 				{ type = "number", value = (term.upper % 4 == 1 or term.lower % 4 == 1) and 1 or -1 },
 				{ type = "legendre", upper = term.lower, lower = term.upper },
@@ -50,9 +61,10 @@ local function termnext(term)
 			return state
 		end
 		for k, v in pairs(factors) do
-			print(k .. "," .. v)
-			if v % 2 == 1 then
-				state[#state + 1] = { type = legendre, upper = k, lower = term.lower }
+			if type(v) == "number" then
+				if v % 2 == 1 then
+					state[#state + 1] = { type = "legendre", upper = k, lower = term.lower }
+				end
 			end
 		end
 		return state
@@ -67,16 +79,69 @@ local function term2str(term)
 		str = [[\legendre{]] .. term.upper .. [[}{]] .. term.lower .. [[}]]
 	end
 	if term.type == "number" then
-		print(term.value)
+		-- print(term.value)
 		str = tostring(term.value)
 	end
 	return str
 end
 local function state2str(state)
 	local str = ""
+	local number = 1
 	for k, v in ipairs(state) do
-		str = str .. term2str(v)
+		if v.type == "legendre" then
+			str = str .. term2str(v)
+		end
+		if v.type == "number" then
+			number = number * v.value
+		end
+	end
+	if str == "" then
+		str = tostring(number)
+	elseif number == -1 then
+		str = "-" .. str
 	end
 	return str
 end
-print(state2str(termnext({ type = "legendre", upper = 6, lower = 13 })))
+local function statenext(oldstate)
+	local newstate = { { type = "number", value = 1 } }
+	local number = 1
+	for _, v in ipairs(oldstate) do
+		local tempstate = termnext(v)
+		for _, y in ipairs(tempstate) do
+			if y.type == "legendre" then
+				newstate[#newstate + 1] = y
+			elseif y.type == "number" then
+				number = number * y.value
+			end
+		end
+	end
+	newstate[1]["value"] = number
+	return newstate
+end
+local function legendre_with_calculation(p, q)
+	if p % q == 0 then
+		return 0
+	end
+	if q == 0 then
+		error("the second argument of legendre should be non-zero!")
+	end
+	if q < 0 then
+		q = -q
+	end
+	local state = { { type = "legendre", upper = p, lower = q } }
+	local calculation = "\\[\n" .. state2str(state)
+	while #state > 1 or state[1].type == "legendre" do
+		state = statenext(state)
+		calculation = calculation .. "\\\\\n=" .. state2str(state)
+	end
+	calculation = calculation .. "\n\\]"
+	return calculation
+end
+for i = 1, 100, 1 do
+	local p = math.random(1, 1000)
+	local q = math.random(1001, 10000)
+	while not isprime(q) do
+		q = math.random(1001, 10000)
+	end
+	print(legendre_with_calculation(p, q))
+end
